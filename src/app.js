f424efb711a8e32b5c3f7e75435e4eb1238cd5a9
anshikayadav11 <anshikayadav2917@gmail.com>
2026-06-13 23:45:43 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -12,8 +12,38 @@ app.post('/notes',(req,res) =>{
     res.status(201).json({
         message:"note created successfully"
     })
-    
+
     console.log(req.body) 
+})
+
+/*GET /notes*/
+app.get('/notes',(req, res) =>
+[
+    res.status(200).json({
+        message:"notes fetched successfully",
+        notes:notes
+
+    })
+])
+/*delete /notes/1*/
+app.delete('/notes/:index',(req, res) =>{
+    const index = req.params.index
+    delete notes[index]
+    
+    res.status(200).json({
+        message:"note deleted successfully"
+    })
+
+})
+
+app.patch("/notes/:index",(req,res) =>{
+    const index =req.params.index
+    const description =req.body.description
+
+    notes[ index ].description =description
+    res.status(200).json({
+        message:"note updated successfully"
+    })
 })
 
 module.exports =app
